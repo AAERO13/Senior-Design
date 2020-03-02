2cae89ebc850e9afc63c29735a4066bf3450ae0e
AAERO13 <42823837+AAERO13@users.noreply.github.com>
2020-03-02 01:22:33 -0500

    Delete scratch.py

--- scratch.py
+++
@@ -1,8 +0,0 @@
-string = 'abcdefg'
-print(string)
-index = 5
-print(string[index])
-
-#string = string[:-index+1] + string[index+1:]
-string = string[0:index] + string[index+1:len(string)]
-print(string)
